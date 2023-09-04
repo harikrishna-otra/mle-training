7725e0bb633b892aee8b96011c4ef294ee807afb
harikrishna-otra <129064998+harikrishna-otra@users.noreply.github.com>
2023-09-04 13:04:28 +0530

    Add files via upload

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -79,7 +79,7 @@ housing = strat_train_set.copy()
 housing.plot(kind="scatter", x="longitude", y="latitude")
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
 
-corr_matrix = housing.corr()
+corr_matrix = housing.corr(numeric_only=True)
 corr_matrix["median_house_value"].sort_values(ascending=False)
 housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
